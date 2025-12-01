4e47f3aa380b2e4aebe3bf10a370183139e3ff52
Mohamed ashraf <muhamed77700m@gmail.com>
2025-12-01 23:46:21 +0200

    logo fix

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,7 +8,7 @@ function Navbar() {
       <div className="max-w-7xl mx-auto px-10 py-5 grid items-center grid-cols-2">
         <div className="relative">
           <Image
-            src={"/MO.svg"}
+            src={"/mo.svg"}
             alt="logo"
             width={100}
             height={100}
