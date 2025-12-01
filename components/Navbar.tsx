4bfb14c18952d6e5e0d9d4ddaac991f820e52ffa
Mohamed ashraf <muhamed77700m@gmail.com>
2025-12-02 01:47:36 +0200

    some responsive change

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,18 +1,20 @@
 import React from "react";
 import Menu from "./Menu";
+import Image from "next/image";
+import Logo from "@/public/mo.svg";
 
 function Navbar() {
   return (
     <div className=" w-full h-20 border-b border-b-slate-500/50 ">
       <div className="max-w-7xl mx-auto px-10 py-5 grid items-center grid-cols-2">
         <div className="relative">
-          <img
-            src={"/mo.svg"}
+          <Image
+            src={Logo}
             alt="logo"
             width={100}
             height={100}
+            priority
             className="w-20"
-           
           />
         </div>
         <Menu />
